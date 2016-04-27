3fa078ef683977b8b4155407fbd6da290d19311b
Marcelo Suzuki <mrsuzuki@gmail.com>
2016-04-26 23:40:08 -0300

    It was added a main method to execute some tests.

--- infrastructure/services/slicer/src/edu/uci/ics/sourcerer/services/slicer/SlicerFactory.java
+++ infrastructure/services/slicer/src/edu/uci/ics/sourcerer/services/slicer/SlicerFactory.java
@@ -17,7 +17,18 @@
  */
 package edu.uci.ics.sourcerer.services.slicer;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Collections;
+
+import javax.swing.JOptionPane;
+
+
+import edu.uci.ics.sourcerer.services.slicer.internal.FelipeDebug;
 import edu.uci.ics.sourcerer.services.slicer.internal.SlicerImpl;
+import edu.uci.ics.sourcerer.services.slicer.internal.SliceImpl;
+import edu.uci.ics.sourcerer.services.slicer.model.Slice;
 import edu.uci.ics.sourcerer.services.slicer.model.Slicer;
 import edu.uci.ics.sourcerer.util.io.arguments.Argument;
 import edu.uci.ics.sourcerer.util.io.arguments.StringArgument;
@@ -26,9 +37,36 @@ import edu.uci.ics.sourcerer.util.io.arguments.StringArgument;
  * @author Joel Ossher (jossher@uci.edu)
  */
 public class SlicerFactory {
-  public static final Argument<String> FILE_SERVER_URL = new StringArgument("file-server-url", "URL for the file server");
-  
-  public static Slicer createSlicer() {
-    return SlicerImpl.create();
-  }
+	
+	public static final Argument<String> FILE_SERVER_URL = new StringArgument("file-server-url", "URL for the file server");
+	
+	public static Slicer createSlicer() {
+		return SlicerImpl.create();
+	}
+
+	
+	
+
+	
+	public static void main(String[] args) {
+		Integer entityID = 8331277;//8537791;//8512696;//8512634;//8347531;
+		long start = System.currentTimeMillis();
+		Slicer slicer = SlicerFactory.createSlicer();
+		Slice result = slicer.slice(Collections.singleton(entityID));
+		SliceImpl si = (SliceImpl)result;
+		FelipeDebug.debug("[SlicerFactory]slice:\n"+si.getInternalEntities().toString().replace(",", "\n").replace("[","").replace("]", ""));
+		byte[] input = result.toZipFile();
+		long end = System.currentTimeMillis();
+		System.out.println("total of "+(end-start)+" ms");
+		String filename = JOptionPane.showInputDialog("Type zip's fqn",System.getProperty("user.home")+File.separator+"slicertest.zip");
+		JOptionPane.showMessageDialog(null, result.getInternalEntities().toString().replace(",", "\n").trim());
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(new File(filename));
+			fos.write(input);
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
 }
